ca79210e1d4b373843892ba54397f7da2b4e8c3a
cabilangan112 <jassencabilangan@gmail.com>
2017-11-21 08:32:11 +0800

    second commit

--- attendace/models.py
+++ attendace/models.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+
 from __future__ import unicode_literals
 
 from django.db import models
@@ -31,3 +31,16 @@ class Department(models.Model):
 	
 	def __str__(self):
 		return  self.Department
+	
+class Post(models.Model):
+    text 			= models.TextField()
+    created_date 	= models.DateTimeField(
+            default	=timezone.now)
+    published_date 	= models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
